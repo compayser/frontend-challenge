cb47b2f204f716f2812bff2712f3f20ca9499bf1
Sergey Semenistyy <compayser@mail.ru>
2017-10-12 16:56:53 +0300

    added empty person entity creation

--- app/app.js
+++ app/app.js
@@ -6,3 +6,27 @@ personsApp.controller('PersonListController', function PersonListController($sco
 		$scope.persons = response.data;
 	}); 
 });
+
+personsApp.controller('NewPersonController', function NewPersonController($scope, $http) {
+	$scope.person = null;
+
+	$scope.createPerson = function() {
+		var id = 100;
+		var data = [
+			{ 	"id" : id, 
+				"photo" : null, 
+				"firstName" : null, 
+				"lastName" : null, 
+				"gender" : null, 
+				"birthDate" : null, 
+				"jobTitle" : null, 
+				"skills" : [],
+				"resume" : null
+			}
+		];
+		$http.post('http://localhost:3000/persons/', data)
+			.then(function successCallback(response) {
+		}); 
+	};
+
+});
